96a5757b2190e15849d02c7e298fac224468a5dd
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-30 15:34:53 +0530

    employee login

--- src/app/employee-login/employee-login.component.ts
+++ src/app/employee-login/employee-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-login',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-login.component.css']
 })
 export class EmployeeLoginComponent {
+  Username=""
+  Password=""
 
+  constructor(private route:Router){}
+
+  readValue = () =>{
+    let data:any = {"Username":this.Username,"Password":this.Password}
+    console.log(data)
+
+    if (this.Username=="1122" && this.Password=="12345" ) {
+      this.route.navigate(['/addcourses'])
+      
+    } else {
+      alert("invalid credentials")
+      
+    }
+
+  }
 }
