ebf62ca3131046b03c7e3a5dcc5093e8e9fdf853
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-30 14:28:43 +0530

    login added

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AdminLoginComponent {
 
+  Username=""
+  Password=""
+
+
+  readValue = () =>{
+    let data:any = {"Username":this.Username,"Password":this.Password}
+    console.log(data)
+  }
+
 }
